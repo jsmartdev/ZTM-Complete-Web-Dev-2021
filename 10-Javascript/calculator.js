d82d363400384d0b60373e8349ba694b5210fd67
Jesse Smart <jsmartdev@protonmail.com>
2021-05-11 18:55:09 -0400

    javascript mini calculator exercise

---
+++ 10-Javascript/calculator.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+let first = prompt("Enter the first number.");
+let second = prompt("Enter the second number.")
+let sum = Number(first) + Number(second);
+let sub = Number(first) - Number(second);
+let mult = Number(first) * Number(second);
+let div = Number(first) / Number(second);
+console.log(sum);
+console.log(sub);
+console.log(mult);
+console.log(div);
+console.log(first + " + " + second + " = " + sum);
