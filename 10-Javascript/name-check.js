27ac744b0ec247024c9124fd9c8af5755f312be2
Jesse Smart <jsmartdev@protonmail.com>
2021-05-11 19:38:17 -0400

    merged if and else if statements into an if a or b statement

--- 10-Javascript/name-check.js
+++ 10-Javascript/name-check.js
@@ -1,10 +1,7 @@
 /* eslint-disable */
 let name = prompt("Type in your name.") 
-if (name === "Jesse") {
+if (name === "Jesse" || name === "Jesse Smart") {
     alert("Greetings master. How can I be of service?")
-}
-else  if (name === "Jesse Smart") {
-    alert("Greetings master. Playing it formal today, I see! What can I do for you?")
 }
 else {
     alert("Begone peasant! I've only one lord and master!")
