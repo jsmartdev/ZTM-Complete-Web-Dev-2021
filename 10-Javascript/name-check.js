a83e19851bc7300481aa469d4def212475514676
Jesse Smart <jsmartdev@protonmail.com>
2021-05-11 19:24:50 -0400

    name check exercise

---
+++ 10-Javascript/name-check.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+let name = prompt("Type in your name.") 
+if (name === "Jesse") {
+    alert("Greetings master. How can I be of service?")
+}
+else {
+    alert("Don't bullshit me! I know your name is Fuckface Turdbritches!")
+}
