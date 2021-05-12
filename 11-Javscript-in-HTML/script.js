f3bf928bcfbe4c0f24a8b2c79ec1d725a55383a9
Jesse Smart <jsmartdev@protonmail.com>
2021-05-11 21:33:16 -0400

    sing function exercise

--- 11-Javscript-in-HTML/script.js
+++ 11-Javscript-in-HTML/script.js
@@ -4,3 +4,25 @@
 if (4 + 3 !== 8) {
     console.log("Welcome Friends!");
 }
+
+function sayHello() {
+    console.log("Hello!")
+}
+
+sayHello();
+
+let helloAgain = function() {
+    console.log("Hello Again!")
+}
+
+helloAgain();
+
+function sing(song) {
+    console.log(song)
+}
+
+sing("SHAMMA!");
+sing("LAMMA!");
+sing("DING!");
+sing("DONG!");
+
