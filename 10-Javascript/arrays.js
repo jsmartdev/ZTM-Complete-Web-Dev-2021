2ae49a7083120a3e2afe48802c1dea26f71dcccb
Jesse Smart <jsmartdev@protonmail.com>
2021-05-21 13:22:22 -0400

    complete array exercise

---
+++ 10-Javascript/arrays.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+
+// using this array,
+
+var array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+// 1. Remove the Banana from the array.
+
+array.splice(0, 1);
+console.log(array);
+
+// 2. Sort the array in order.
+
+array.sort();
+console.log(array);
+
+// 3. Put "Kiwi" at the end of the array.
+
+array.push("Kiwi");
+console.log(array);
+
+// 4. Remove "Apples" from the array.
+
+array.splice(0, 1);
+console.log(array);
+
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse
+// the current Array i.e. ['a', 'c', 'b'] becomes ['b', 'c', 'a'])
+
+array.reverse();
+console.log(array);
+
+//you should have at the end:
+//["Kiwi", "Oranges", "Blueberries"]
+
+// urray2 = using this array,
+// var a ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+// access "Oranges"
+
+var array2 =  ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(array2[1][1][0]);
