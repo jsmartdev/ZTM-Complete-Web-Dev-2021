e266d0e650de33e2fe682728ce672a944dac5dd5
Jesse Smart <jsmartdev@protonmail.com>
2021-05-21 13:54:53 -0400

    complete objects and arrays exercise

---
+++ 10-Javascript/objects&arrays.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+
+// Create an object and an array which we will use in our facebook exercise. 
+
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+let user = {
+    username: "Broseph Dudeman",
+    password: "broseph92",
+};
+
+// 2. Create an array which contains the object you have made above and name the array "database".
+
+let database = [
+    {
+    username: "Broseph Dudeman",
+    password: "broseph92",
+    }
+];
+
+// 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+let newsfeed = [
+    {
+        username: "John Wells",
+        timeline: "Tuesday"
+    },
+    {
+        username: "Brenda Knight",
+        timeline: "Monday"
+    },
+    {
+        username: "Judy Brown",
+        timeline: "Friday"
+    }
+];
