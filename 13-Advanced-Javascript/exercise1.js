93a24e94d752b2adb5ffa82dd0400479e4f798c4
Jesse Smart <jsmartdev@protonmail.com>
2021-08-19 16:29:43 -0400

    answered all questions in exercise

--- 13-Advanced-Javascript/exercise1.js
+++ 13-Advanced-Javascript/exercise1.js
@@ -1,7 +1,9 @@
 /* eslint-disable */
 
 // For all of these, what is the value of a when the function gets called with the alert()
-// #1
+
+// #1 Answer: 5
+
 function q1() {
     var a = 5;
     if(a > 1) {
@@ -10,7 +12,9 @@ function q1() {
     alert(a);
 }
 
-//#2
+
+//#2 Answer: 0
+
 var a = 0;
 function q2() {
     a = 5;
@@ -21,7 +25,8 @@ function q22() {
 }
 
 
-//#3
+//#3 Answer: undefined
+
 function q3() {
     window.a = "hello";
 }
@@ -31,14 +36,16 @@ function q32() {
     alert(a);
 }
 
-//#4
+//#4 Answer: test
+
 var a = 1;
 function q4() {
     var a = "test";
     alert(a);
 }
 
-//#5
+//#5 Answer: 5, 2
+
 var a = 2;
 if (true) {
     var a = 5;
