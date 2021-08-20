57c6bc1d4196b298390c9ac5a706abfc9d63ec5f
Jesse Smart <jsmartdev@protonmail.com>
2021-08-20 17:53:26 -0400

    answer problem 2 of exercise 5

--- 13-Advanced-Javascript/exercise5.js
+++ 13-Advanced-Javascript/exercise5.js
@@ -42,19 +42,19 @@ console.log(newArray);
 
 //Create an array using map that has all the usernames with a "? to each of the usernames
 
+const mapArray = array.map(user => {
+	let { username } = user;
+	return username + "?";
+})
+console.log(mapArray);
 
 //Filter the array to only include users who are on team: red
+
 
 
 //Find out the total score of all users using reduce
 
 // (1), what is the value of i?
 // (2), Make this map function pure:
-const arrayNum = [1, 2, 4, 5, 8, 9];
-const newArray = arrayNum.map((num, i) => {
-	console.log(num, i);
-	alert(num);
-	return num * 2;
-})
 
 //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
