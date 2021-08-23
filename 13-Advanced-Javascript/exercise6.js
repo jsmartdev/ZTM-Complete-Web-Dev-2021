bf6e35815e9f056338a18d868c5bc74128df0352
Jesse Smart <jsmartdev@protonmail.com>
2021-08-23 18:53:25 -0400

    complete exercise 6

--- 13-Advanced-Javascript/exercise6.js
+++ 13-Advanced-Javascript/exercise6.js
@@ -15,3 +15,23 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+
+}
+
+class Mammal extends Animal {
+    constructor(name, type, color) {
+        super(name, type, color);
+    }
+    sound() {
+        console.log(`Moooo! I'm ${this.name}, a ${this.color} ${this.type}!`);
+    }
+}
+
+const cow = new Mammal("Harvy", "cow", "spotted");
